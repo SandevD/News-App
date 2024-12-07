a62306687e2ef15551df2b86b766e64101a535d7
sandevD <sandev.net@gmail.com>
2024-12-08 05:02:53 +0530

    ✨ Update API client to support category parameter in getTopHeadlines method

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -4,11 +4,17 @@ import 'package:dio/dio.dart';
 class ApiClient {
   final Dio _dio = Dio(BaseOptions(
     baseUrl: 'https://newsapi.org/v2/',
-    headers: {'Authorization': 'Bearer 33331ff120104c2f9bed78b1817b4ab6'},
+    headers: {'Authorization': 'Bearer c6bdbe89c47e48ab99382d58402330e5'},
   ));
 
-  Future<Response> getTopHeadlines() async {
-    return await _dio.get('top-headlines', queryParameters: {'country': 'us'});
+  Future<Response> getTopHeadlines({required String category}) async {
+    return await _dio.get(
+      'top-headlines',
+      queryParameters: {
+        'country': 'us',
+        'category': category,
+      },
+    );
   }
 
   Future<Response> getPopularNews({required String category}) async {
