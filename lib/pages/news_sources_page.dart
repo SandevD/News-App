f18c1e4019be2898923683778b642684119c9d76
sandevD <sandev.net@gmail.com>
2024-12-08 02:06:53 +0530

    ✨ Add NewsSourcesPage to display a list of news sources fetched from the API

---
+++ lib/pages/news_sources_page.dart
@@ -0,0 +1,51 @@
+// lib/news_sources_page.dart
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import '../services/api_client.dart';
+
+class NewsSourcesPage extends StatefulWidget {
+  const NewsSourcesPage({super.key});
+
+  @override
+  _NewsSourcesPageState createState() => _NewsSourcesPageState();
+}
+
+class _NewsSourcesPageState extends State<NewsSourcesPage> {
+  final ApiClient _apiClient = ApiClient();
+  List<dynamic> _sources = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchNewsSources();
+  }
+
+  void _fetchNewsSources() async {
+    final response = await _apiClient.getNewsSources();
+    setState(() {
+      _sources = response.data['sources'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News Sources'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+        itemCount: _sources.length,
+        itemBuilder: (context, index) {
+          final source = _sources[index];
+          return ListTile(
+            title: Text(source['name']),
+            subtitle: Text(source['description'] ?? 'No description available'),
+          );
+        },
+      ),
+    );
+  }
+}
